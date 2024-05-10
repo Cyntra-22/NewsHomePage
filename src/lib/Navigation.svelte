78c43258d2f1299d55300812c25db1ea8f56bead
Cyntra-22 <140875968+Cyntra-22@users.noreply.github.com>
2024-05-10 17:55:09 +0700

    Added navigation bar

--- src/lib/Navigation.svelte
+++ src/lib/Navigation.svelte
@@ -2,6 +2,37 @@
 
 </script>
 <style>
+       
+        ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        }
 
+        li {
+        float: left;
+        }
+
+        li a {
+        display: block;
+        color: black;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+        }
+
+        li a:hover {
+        background-color: #111;
+        }
 </style>
 
+<div class="nav-container">
+    <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#news">News</a></li>
+        <li><a href="#contact">Popular</a></li>
+        <li><a href="#about">Trending</a></li>
+        <li><a href="#about">Categories</a></li>
+    </ul>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,46 @@
 <script>
+  import Navigation from "$lib/Navigation.svelte";
+
 
 </script>
 <style>
+    :root{
+		--font-color: hsl(233, 8%, 79%);
+        --text-color: hsl(236, 13%, 42%);
+		--font: 'Inter', sans-serif;
+        --button-color: hsl(35, 77%, 62%);
+		
+	}
+
+	:global(body){
+		
+		margin: 0 auto;
+		font-family: var(--font); 
+		 
+	}
+    .main-container{
+        width: 80%;
+        background-color: aqua;
+        margin: 0 auto;
+
+    }
+
+    .header{
+        display: flex;
+        justify-content: space-between;
+    }
 
 </style>
 
 <body>
-    <header>
-        <img src="/logo.svg" alt="logo"/>
-        
-    </header>
+    <div class="main-container">
+        <div class="header">
+            <img src="/logo.svg" alt="logo"/>
+            <Navigation/>
+        </div>
+    </div>
+    
+          
 </body>
     
 
