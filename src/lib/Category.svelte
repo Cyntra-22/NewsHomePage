7c138e468d335fb199c32389ff592369b29d5c06
Cyntra-22 <140875968+Cyntra-22@users.noreply.github.com>
2024-05-14 16:01:28 +0700

    Added categories page

---
+++ src/lib/Category.svelte
@@ -0,0 +1,29 @@
+<style>
+    .category-container{
+        display: flex;
+        padding-bottom: 10px;
+    }
+     img{
+        width: 25%;  
+        margin-right: 30px;
+    }
+    h3{
+        padding-top: 15px;
+    }
+    a{
+        text-decoration: none;
+        color: black;
+    }
+    a:hover{
+        color: var(--button-color);
+    }
+  
+</style>
+
+<div class="category-container">
+
+        <img src="https://www.adonimedia.com.au/wp-content/uploads/2023/01/why-its-so-hard-to-get-good-news-in-the-media.jpg.webp" alt="new title"/> 
+    <div>
+        <a href="/"><h3>Michael Cohen's Testimony Appears 'Credible,' Legal Analysts Say</h3></a>
+    </div>
+</div>
--- src/routes/categories/+page.svelte
+++ src/routes/categories/+page.svelte
@@ -1,1 +1,124 @@
-<h1>Categories Page</h1>
+<script>
+    import Category from "$lib/Category.svelte";
+</script>
+<style>
+    .list-container{
+        display: flex;
+        justify-content: space-between;
+        padding-bottom: 35px;
+        border-bottom: 1px solid var(--text-color);
+    }
+    a{
+        text-decoration: none;
+        color: var(--button-color);
+    }
+    li{
+        list-style-type: none;
+        
+    }
+    h1{
+        margin-bottom: 40px;
+        text-align: center;
+        background-color: var(--button-color);
+        padding: 15px;
+        letter-spacing: 4px;
+        color: white;
+    }
+
+    a:hover{
+        color: black;
+    }
+
+   
+</style>
+
+<div>
+    <h1>CATEGORIES</h1>
+    <div class="list-container">
+         <li><a href="#content-policy">Content Policy</a></li> |
+        <li><a href="#freedom-of-expression">Freedom of Expression</a></li> |
+        <li><a href="#human-right">Human Right</a></li> |
+        <li><a href="#legal-and-regulatory">Legal and Regulatory</a></li> |
+        <li><a href="#socioculture">Socioculture</a></li> |
+        <li><a href="#laws-business">Laws and Business</a></li> 
+    </div>
+    <div>
+        <h2>Related News</h2>
+        <Category/><hr/>
+        <Category/><hr/>
+        <Category/><hr/>
+        <Category/><hr/>
+    </div>
+   
+    <div id="content-policy">
+        <h2>Content Policy</h2>
+        <div style="display:flex;">
+            <Category/>
+            <Category/>
+        </div>
+        <div style="display:flex;">
+            <Category/>
+            <Category/>
+        </div>
+    </div>
+    <hr/>
+     <div id="freedom-of-expression">
+        <h2>Freedom of Expression</h2>
+        <div style="display:flex;">
+            <Category/>
+            <Category/>
+        </div>
+        <div style="display:flex;">
+            <Category/>
+            <Category/>
+        </div>
+    </div>
+    <hr/>
+     <div id="human-right">
+        <h2>Human Right</h2>
+        <div style="display:flex;">
+            <Category/>
+            <Category/>
+        </div>
+        <div style="display:flex;">
+            <Category/>
+            <Category/>
+        </div>
+    </div>
+    <hr/>
+     <div id="legal-and-regulatory">
+        <h2>Legal and Regulatory</h2>
+        <div style="display:flex;">
+            <Category/>
+            <Category/>
+        </div>
+        <div style="display:flex;">
+            <Category/>
+            <Category/>
+        </div>
+    </div>
+    <hr/>
+     <div id="socioculture">
+        <h2>Socioculture</h2>
+        <div style="display:flex;">
+            <Category/>
+            <Category/>
+        </div>
+        <div style="display:flex;">
+            <Category/>
+            <Category/>
+        </div>
+    </div>
+    <hr/>
+    <div id="laws-business">
+        <h2>Laws and Business</h2>
+        <div style="display:flex;">
+            <Category/>
+            <Category/>
+        </div>
+        <div style="display:flex;">
+            <Category/>
+            <Category/>
+        </div>
+    </div>    
+</div>
